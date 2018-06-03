2d2a26c980c203f841da8bf68b0d7a1f8c8c7578
Noah Greer <noah.greer@gmail.com>
2018-06-02 20:44:17 -0700

    Update anapyzercontroller.py

--- anapyzercontroller.py
+++ anapyzercontroller.py
@@ -20,6 +20,7 @@ class AnaPyzerController:
         self.update_view()
         self.view.mainloop()
 
+    # Function for initializing the view with values from the model and registering listeners
     def init_view(self):
         # Set the available options for the view's options menu
         self.view.set_log_type_options([log_type.value for log_type in AcceptedLogTypes])
@@ -38,6 +39,32 @@ class AnaPyzerController:
         self.view.set_graph_mode_option_changed_listener(self.graph_mode_option_changed)
         self.view.set_report_mode_option_changed_listener(self.report_mode_option_changed)
 
+    # Function for updating the state of the view based on what has been set in the model
+    def update_view(self):
+        # Set the input and output file paths to those set in the model
+        self.view.set_in_file_path(str(self.model.get_in_file_path()))
+        self.view.set_out_file_path(str(self.model.get_out_file_path()))
+
+        # Hide all optional widgets by default
+        self.view.hide_graph_mode_option_menu_widgets()
+        self.view.hide_report_mode_option_menu_widgets()
+        self.view.hide_out_file_path_widgets()
+        self.view.disable_open_file_button()
+
+        # Show only the widgets that pertain to the current parse mode
+        if self.model.get_file_parse_mode() == FileParseModes.GRAPH:
+            self.view.show_graph_mode_option_menu_widgets()
+            if self.model.in_file_path_is_valid():
+                self.view.enable_open_file_button()
+        elif self.model.get_file_parse_mode() == FileParseModes.REPORT:
+            self.view.show_report_mode_option_menu_widgets()
+            if self.model.in_file_path_is_valid():
+                self.view.enable_open_file_button()
+        elif self.model.get_file_parse_mode() == FileParseModes.CSV:
+            self.view.show_out_file_path_widgets()
+            if self.model.in_file_path_is_valid() and self.model.out_file_path_is_valid():
+                self.view.enable_open_file_button()
+        
     # Listener for when the log type option menu has an item selected
     def log_type_option_changed(self, value):
         self.model.set_log_type(value)
@@ -84,7 +111,6 @@ class AnaPyzerController:
     # Function for handling when the "Open" button is pressed
     def open_file_button_clicked(self):
         parse_mode = self.model.get_file_parse_mode()
-        graph_mode = self.model.get_graph_mode()
 
         if parse_mode == FileParseModes.GRAPH:
             try:
@@ -95,7 +121,7 @@ class AnaPyzerController:
 
             # model.get_graph_data_split will check whether the graph data is split by date/time/any other delimiter
             # and allow multiple graphs to be created if it is
-            print(self.model.get_graph_data_split())
+            # print(self.model.get_graph_data_split())
             if len(self.model.get_graph_data_split()) > 0:
                 for value in self.model.get_graph_data_split():
                     self.view.display_graph_view(self.model.get_graph_data_split_keys(value),
@@ -109,11 +135,6 @@ class AnaPyzerController:
                                              self.model.get_graph_data_x_label(),
                                              self.model.get_graph_data_y_label(),
                                              self.model.get_graph_data_title())
-
-            # If we are in graph simultaneous connections
-        elif graph_mode == GraphModes.SIMUL_CON:
-            #self.view.display_graph_view()
-            pass
 
         elif parse_mode == FileParseModes.REPORT:
             report_mode = self.model.get_report_mode()
@@ -150,29 +171,3 @@ class AnaPyzerController:
     # Function for displaying a success message in the view
     def success_event_listener(self, message):
         self.view.display_success_message(message)
-
-    # Function for updating the state of the view based on what has been set in the model
-    def update_view(self):
-        # Set the input and output file paths to those set in the model
-        self.view.set_in_file_path(str(self.model.get_in_file_path()))
-        self.view.set_out_file_path(str(self.model.get_out_file_path()))
-
-        # Hide all optional widgets by default
-        self.view.hide_graph_mode_option_menu_widgets()
-        self.view.hide_report_mode_option_menu_widgets()
-        self.view.hide_out_file_path_widgets()
-        self.view.disable_open_file_button()
-
-        # Show only the widgets that pertain to the current parse mode
-        if self.model.get_file_parse_mode() == FileParseModes.GRAPH:
-            self.view.show_graph_mode_option_menu_widgets()
-            if self.model.in_file_path_is_valid():
-                self.view.enable_open_file_button()
-        elif self.model.get_file_parse_mode() == FileParseModes.REPORT:
-            self.view.show_report_mode_option_menu_widgets()
-            if self.model.in_file_path_is_valid():
-                self.view.enable_open_file_button()
-        elif self.model.get_file_parse_mode() == FileParseModes.CSV:
-            self.view.show_out_file_path_widgets()
-            if self.model.in_file_path_is_valid() and self.model.out_file_path_is_valid():
-                self.view.enable_open_file_button()
