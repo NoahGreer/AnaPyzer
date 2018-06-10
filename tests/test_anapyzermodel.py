1577654e6d5e4fa245657f6790e598fa9947be75
Noah Greer <noah.greer@gmail.com>
2018-06-10 13:17:53 -0700

    Added tests for setting the input and output file path in the model.

--- tests/test_anapyzermodel.py
+++ tests/test_anapyzermodel.py
@@ -2,6 +2,8 @@ import unittest
 import unittest.mock
 # Import the AnaPyzerModel class
 from anapyzermodel import AnaPyzerModel
+# Import the pathlib library for cross platform file path abstraction
+import pathlib
 
 class TestAnaPyzerModelMethods(unittest.TestCase):
     def setUp(self):
@@ -19,5 +21,17 @@ class TestAnaPyzerModelMethods(unittest.TestCase):
 
     # If a file path string is passed, then the model should store that file path
     def test_set_in_file_path_string(self):
-        self.model.set_in_file_path('')
-        self.assertEqual(str(self.model.DEFAULT_FILE_PATH), self.model.get_in_file_path())
+        path = pathlib.Path.home()
+        self.model.set_in_file_path(path)
+        self.assertEqual(str(path), self.model.get_in_file_path())
+
+    # If a blank string is passed, then the model should just set the path to the default path
+    def test_set_out_file_path_blank(self):
+        self.model.set_out_file_path('')
+        self.assertEqual(str(self.model.DEFAULT_FILE_PATH), self.model.get_out_file_path())
+
+    # If a file path string is passed, then the model should store that file path
+    def test_set_out_file_path_string(self):
+        path = pathlib.Path.home()
+        self.model.set_out_file_path(path)
+        self.assertEqual(str(path), self.model.get_out_file_path())
